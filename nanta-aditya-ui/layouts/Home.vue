7f0babf1a8ab8faaaeacf6315ab473602a0e75d1
pramuditya anantanur <pramuditza@gmail.com>
2018-08-04 12:59:57 +0700

    add home layout

---
+++ nanta-aditya-ui/layouts/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">    
+    <!--HEADER-->
+    <Header></Header>
+    <nuxt/>
+    <!--FOOTER-->
+  </div>
+</template>
+<script>
+import Header from '~/components/Home/Header.vue'
+
+export default {
+  components: {
+    Header,    
+  },  
+}
+</script>
