941de1675537af7823d5bc4773d9224fcc4f5580
pramuditya anantanur <pramuditza@gmail.com>
2018-09-01 12:01:40 +0700

    update meta description in contact

--- nanta-aditya-ui/pages/Contact.vue
+++ nanta-aditya-ui/pages/Contact.vue
@@ -22,12 +22,12 @@ export default {
         return {
             title: "Nanta Aditya\'s website | Contact",
             meta: [
-                { hid: 'description', name: 'description', content: 'Nanta Aditya\'s Contact | This is where you can reach me, contact me if you have something to talk to me.' },
+                { hid: 'description', name: 'description', content: 'Nanta Aditya\'s Contact | This is where you can reach me, feel free to talk to me.' },
                 { hid: 'keywords', name: 'keywords', content: 'Nanta Aditya, Nanta Aditya\'s Website, Nanta Aditya\'s Profile, Nanta Aditya\'s Contact'},      
                 { hid: 'og:url', property: 'og:url', content: 'http://www.nantaaditya.com/contact'},
                 { hid: 'og:type', property: 'og:type', content: 'website'},
                 { hid: 'og:title', property: 'og:title', content: 'Nanta Aditya\'s website | Contact'},
-                { hid: 'og:description', property: 'og:description', content: 'Nanta Aditya\'s Contact | This is where you can reach me, contact me if you have something to talk to me.'},
+                { hid: 'og:description', property: 'og:description', content: 'Nanta Aditya\'s Contact | This is where you can reach me, feel free to talk to me.'},
                 { hid: 'og:image', property: 'og:image', content: 'localhost:3000/icon.png'},                                
             ],
             link: [
