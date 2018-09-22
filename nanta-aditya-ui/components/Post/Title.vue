a3d9182f95670788918c107d0c7e7e1df98aa8ed
pramuditya anantanur <pramuditza@gmail.com>
2018-09-22 23:25:43 +0700

    add post component

---
+++ nanta-aditya-ui/components/Post/Title.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <ol class="breadcrumb" style="background: #fff">
+                <li><nuxt-link to="/blog">Blog</nuxt-link></li>
+                <li class="active">{{title}}</li>
+            </ol>
+        </div>                
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Title",
+    props: {
+        title:{
+            type: String
+        }
+    }
+}
+</script>
