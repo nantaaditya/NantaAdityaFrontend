f9cdf3a6ff20039af0fb1a9086f070e6cdcb9b0b
pramuditya anantanur <pramuditza@gmail.com>
2018-09-30 09:11:20 +0700

    update blog component, fix search

--- nanta-aditya-ui/components/Blog/Search.vue
+++ nanta-aditya-ui/components/Blog/Search.vue
@@ -10,17 +10,16 @@
           <div class="title">
             <a target="_blank" href="/">Nanta Aditya</a>
           </div>
-          <div class="desc">Enjoy the post.</div>
+          <div class="desc">Happy reading.</div>
         </div>
         <div class="bottom">
           <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <div class="input-group">
-                <input type="text" class="form-control" placeholder="search" v-model="searchKeyword" />
-                <span class="input-group-addon">
-                  <i class="fa fa-search"></i>
-                </span>
-              </div>
+              <blockquote style="color:#666">
+                The more that you read, the more things you will know. <br/>
+                The more that you learn, the more places you'll go.
+                <footer class="blockquote-footer">Dr. Seuss in <cite title="Source Title">I Can Read With My Eyes Shut!</cite></footer>
+              </blockquote>
             </div>
           </div>
         </div>
