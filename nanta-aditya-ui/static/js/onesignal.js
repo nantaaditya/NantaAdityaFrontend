a8634e9f416105f5c1ed6ffd401744bc12c1ed80
pramuditya anantanur <pramuditza@gmail.com>
2018-11-03 20:11:35 +0700

    add one signal module

---
+++ nanta-aditya-ui/static/js/onesignal.js
@@ -0,0 +1,9 @@
+this.$OneSignal.push(() => {
+    this.$OneSignal.isPushNotificationsEnabled((isEnabled) => {
+    if (isEnabled) {
+      console.log('Push notifications are enabled!')
+    } else {
+      console.log('Push notifications are not enabled yet.')
+    }
+  })
+})
